c9bed036129105233a8498f2c8f4db55a5986f46
jolly01008 <jollgmll292@gmail.com>
2024-12-30 14:07:14 +0800

    add socket io & react-modal for chat function

--- src/pages/UserOneGroup/UserOneGroup.jsx
+++ src/pages/UserOneGroup/UserOneGroup.jsx
@@ -3,10 +3,12 @@
 import { getUserOneGroup } from '../../api/user';
 
 // hook
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
+import Modal from 'react-modal';
+import Swal from 'sweetalert2'
 
-//components
+import { io } from 'socket.io-client';
 
 //scss
 import styles from "./UserOneGroup.module.scss";
@@ -21,6 +23,12 @@ export default function UserOneGroup() {
   // const [user, setUser] = useState(null);
   const [gpData, setGpData] = useState(null);
 
+  // 聊天相關
+  const [socket, setSocket] = useState(null);   // 用來存儲 WebSocket 連接
+  const [isOpen, setIsOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const messageContainerRef = useRef(null);  // 使用 ref 引用 messageContainer (取代document.getElementById)
+
   // const token = localStorage.getItem("token");
 
   useEffect(() => {
@@ -29,7 +37,6 @@ export default function UserOneGroup() {
       try {
         //後端拿到的資料存到 user
         const fetchGroup = await getUserOneGroup(userId, gpId);
-        console.log('fetchGroup:', fetchGroup)
         setGpData(fetchGroup.data);
       } catch (error) {
         console.error(error);
@@ -39,11 +46,128 @@ export default function UserOneGroup() {
     getUserAsync();
 
   }, [userId, gpId]);
-  console.log('gpData狀態有改變嗎:', gpData)
+
+  // 點擊進入聊天室按鈕。於此創建 WebSocket 連接
+  const handleJoinRoom = () => {
+    const socketInstance = io("http://localhost:3001");
+    setSocket(socketInstance); // 存儲 socket 實例
+
+    setIsOpen(true); // 顯示 Modal
+
+    // 加入聊天室
+    socketInstance.emit("joinRoom", { gpMembers: gpData.gpMembers });
+
+    // 監聽其他成員加入聊天室
+    socketInstance.on("otherJoined", data => {
+      const { broadcast } = data
+
+      if (broadcast) {
+        Swal.fire({
+          position: "top",
+          text: broadcast,
+          confirmButtonText: 'OK',
+          timer: 2500
+        })
+        appendMessage(`( ${broadcast} )`)
+      }
+    })
+
+    socketInstance.on("room-message", data => {
+      const needBroadcastMsg = data.message
+      appendMessage(`${data.fromSocketId} : ${needBroadcastMsg}`)
+    })
+
+    socketInstance.on("user-disconnected", data => {
+      appendMessage(`( ${data} )`)
+    })
+  };
+
+  // 點擊退出聊天室按鈕
+  const handleCloseChat = () => {
+    if (socket) {
+      socket.disconnect(); // 只需要呼叫 socket.disconnect() 來關閉連接
+      setSocket(null); // 清空 socket 狀態
+    }
+
+    setIsOpen(false); // 關閉 Modal
+  };
+
+  // 傳送聊天訊息
+  const handleSendMessage = () => {
+    const roomMembers = gpData.gpMembers
+
+    appendMessage(`You : ${message}`)
+    socket.emit('send-room-message', ({ roomMembers, message }))
+    setMessage('');
+  };
+
+  // 把從伺服器接emit過來、所收到的聊天訊息加在畫面container 的function
+  function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+
+    // 添加 SCSS 類
+    messageElement.classList.add(styles.messageLine);
+
+    messageContainerRef.current.appendChild(messageElement); // 使用 ref 來操作 DOM
+  }
+
   return (
     <div>
       <div className={styles.container}>
-        {/* <div className={styles.selfIntroContainer}>
+
+        <div className={styles.bookingContainer}>
+          {gpData && <h3>群組名稱 : {gpData.gpName}</h3>}
+          <button className={styles.openChatBtn} onClick={handleJoinRoom}> 傳訊息給對方 </button>
+
+          <div className={styles.newBooking}>
+            <h5 className={styles.title} style={{ fontWeight: 'bold' }}>該群組的紀錄項目</h5>
+            <div className={styles.bookingTitle}>
+              <h6 className={styles.bookingItem} style={{ marginTop: '6.5px' }}>項目</h6>
+              <h6 className={styles.bookingItemDate} style={{ marginTop: '6.5px' }}>先付者</h6>
+              <h6 className={styles.bookingItemPrice} style={{ marginTop: '6.5px' }}>欠款者</h6>
+              <h6 className={styles.bookingItemTenant} style={{ marginTop: '6.5px' }}>價格</h6>
+              <h6 className={styles.bookingItemBeds} style={{ marginTop: '6.5px' }}>時間</h6>
+            </div>
+            <div key={'booking.id'} className={styles.bookingList}>
+              <h6 className={styles.bookingItem} style={{ marginTop: '6.5px' }}>機票錢</h6>
+              <h6 className={styles.bookingItemDate} style={{ marginTop: '6.5px' }}>AAA</h6>
+              <h6 className={styles.bookingItemPrice} style={{ marginTop: '6.5px' }}>BBB</h6>
+              <h6 className={styles.bookingItemTenant} style={{ marginTop: '6.5px' }}>$ 14000</h6>
+              <h6 className={styles.bookingItemBeds} style={{ marginTop: '6.5px' }}>$ 14000</h6>
+            </div>
+          </div>
+        </div>
+
+        <Modal
+          isOpen={isOpen}
+          contentLabel="Chat Window"
+          ariaHideApp={false} // For accessibility purposes (optional)
+          className={styles.chatModal}
+          overlayClassName={styles.chatOverlay}
+        >
+          <h2>開始聊天</h2>
+
+          <div className={styles.chatBody}>
+            {/* <div id="messageContainer"></div> */}
+            <div ref={messageContainerRef}></div>
+            <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Type your message..."
+              rows="5"
+              className={styles.messageInput}
+            />
+            <button className={styles.sendBtn} onClick={handleSendMessage}>Send</button>
+          </div>
+          <div className={styles.chatHeader}>
+            <button className={styles.closeBtn} onClick={handleCloseChat}>結束對話</button>
+          </div>
+
+        </Modal>
+
+        <div>
+          {/* <div className={styles.selfIntroContainer}>
           <div className={styles.avatar}>
             <img className={styles.img} src={landlordData.avatar} alt="" />
           </div>
@@ -67,33 +191,7 @@ export default function UserOneGroup() {
             </a>
           </div>
         </div> */}
-
-        <div className={styles.bookingContainer}>
-          {gpData && <h3>群組名稱 : {gpData.gpName}</h3>}
-          <button>傳訊息給對方</button>
-          <div className={styles.newBooking}>
-            <h5 className={styles.title} style={{ fontWeight: 'bold' }}>該群組的紀錄項目</h5>
-            <div className={styles.bookingTitle}>
-              <h6 className={styles.bookingItem} style={{ marginTop: '6.5px' }}>項目</h6>
-              <h6 className={styles.bookingItemDate} style={{ marginTop: '6.5px' }}>先付者</h6>
-              <h6 className={styles.bookingItemPrice} style={{ marginTop: '6.5px' }}>欠款者</h6>
-              <h6 className={styles.bookingItemTenant} style={{ marginTop: '6.5px' }}>價格</h6>
-              <h6 className={styles.bookingItemBeds} style={{ marginTop: '6.5px' }}>時間</h6>
-            </div>
-            <div key={'booking.id'} className={styles.bookingList}>
-              <h6 className={styles.bookingItem} style={{ marginTop: '6.5px' }}>機票錢</h6>
-              <h6 className={styles.bookingItemDate} style={{ marginTop: '6.5px' }}>AAA</h6>
-              <h6 className={styles.bookingItemPrice} style={{ marginTop: '6.5px' }}>BBB</h6>
-              <h6 className={styles.bookingItemTenant} style={{ marginTop: '6.5px' }}>$ 14000</h6>
-              <h6 className={styles.bookingItemBeds} style={{ marginTop: '6.5px' }}>$ 14000</h6>
-              {/* {booking.bedRecords ?
-                <h6 className={styles.bookingItemBeds}>{booking.bedRecords.join(', ')} </h6> :
-                <h6 className={styles.bookingItemBeds}> (套房不分床位)</h6>} */}
-            </div>
-          </div>
         </div>
-
-
       </div>
     </div>
   )
